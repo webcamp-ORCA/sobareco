da5826c95ab77f1358633b709377154187de8c26
tatsuro-kyoizumi <tatsu.sd.us@gmail.com>
2019-10-14 17:18:08 +0900

    routing..

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
   # ここから下はCustomersのルーティングになります
-  
+  get '/admin/customers' => "customers#index"
+  get '/admin/customers/:id' => "customers#show"
+  get '/admin/customers/:id/purchase_history' => "customers#purchase_history"
+  get '/admin/customers/:id' => "customers#edit"
+  get '/admin/customers/:id/destroy_confirm' => "customers#destroy_confirm"
+  get '/admin/customers/:id' => "customers#destroy"
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
