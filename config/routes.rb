8760a2e61532b3fc4a607ef3f3d1d34487744cae
ryuta <ryuta046@gmail.com>
2019-10-16 02:43:55 +0000

    管理者画面作成しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
 
-    namespace :admin do
-   resources :arrival_management
+  namespace :admin do
+    resources :customers
+    resources :orders
+    resources :artists
+    resources :labels
+    resources :genres
+    resources :arrival_managements
   end
+
+ namespace :admin do
+  devise_for :admin_users,controllers: {
+      sessions: 'admin/admin_users/sessions',
+      registrations: 'admin/admin_users/registrations',
+      passwords: 'admin/admin_users/passwords'
+    }
+  end
+
   devise_for :customers
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_135545) do
+ActiveRecord::Schema.define(version: 2019_10_14_202653) do
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
 
   create_table "arrival_managements", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "arrival_date"
     t.integer "arrival_count"
+    t.integer "product_id"
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "artist_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
@@ -34,6 +53,18 @@ ActiveRecord::Schema.define(version: 2019_10_13_135545) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "genre_name"
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "label_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
