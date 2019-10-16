4f277248b8bede014342cd0f42db09947fdce1b9
ryuta <ryuta046@gmail.com>
2019-10-16 04:58:29 +0000

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   namespace :admin do
     resources :customers
     resources :orders
@@ -20,7 +21,7 @@ Rails.application.routes.draw do
   devise_for :customers
 
 
-  
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
