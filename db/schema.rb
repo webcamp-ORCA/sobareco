11a8cd21da3510d5b6d48d57f157b05ea15d6e30
tatsuro-kyoizumi <tatsu.sd.us@gmail.com>
2019-10-26 09:02:48 +0000

    again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_070952) do
+ActiveRecord::Schema.define(version: 2019_10_24_092154) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -44,11 +44,11 @@ ActiveRecord::Schema.define(version: 2019_10_24_070952) do
   end
 
   create_table "cards", force: :cascade do |t|
-    t.integer "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
